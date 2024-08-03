b52382a085ec41f1a9567eb24fcc6551559b263f
Sid9511 <104961838+Sid9511@users.noreply.github.com>
2024-08-03 17:52:25 +0530

    Final Deployment

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,12 @@ app.use((req, res, next) => {
 });
 
 // Handle preflight requests
-app.options('*', cors());
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.send();
+});
 
 app.get('/', async (req, res) => {
     try {
